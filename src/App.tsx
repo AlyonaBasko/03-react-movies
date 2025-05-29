3b1be1f4a9c1fbe0811586f6bef7a4a264c3fbca
alona <alenabask93@gmail.com>
2025-05-29 22:04:09 +0200

    додати всі компоненти

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
 import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-
+import css from "./App.module.css";
 import SearchBar from './components/SearchBar/SearchBar';
+import MovieGrid from './components/MovieGrid/MovieGrid';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import MovieModal from './components/MovieModal/MovieModal';
 import fetchMovies from './services/movieService';
 import { type Movie } from './types/movie';
+
+
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const handleSearch = async (query: string) => {
     setMovies([]);
+    setError(false); 
     setIsLoading(true);
-
+  
     try {
       const results = await fetchMovies({ query });
-
+  
       if (results.length === 0) {
         toast.error('No movies found for your request.');
         return;
       }
 
-      setMovies(results);
+      await new Promise((resolve) => setTimeout(resolve, 400));
+        setMovies(results);
     } catch (error) {
+      setError(true);
       toast.error('Something went wrong. Please try again.');
       console.error('Fetch error:', error);
     } finally {
@@ -30,24 +41,28 @@ export default function App() {
     }
   };
 
+  const handleMovieSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
   return (
-    <>
+    <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
       <Toaster position="top-right" />
 
-      {isLoading && <p style={{ textAlign: 'center' }}>Loading...</p>}
+      {isLoading && <Loader />}
 
       <main>
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>
-              <h3>{movie.title}</h3>
-              <p>Rating: {movie.vote_average}</p>
-              <p>{movie.release_date}</p>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </>
+      {error ? (
+    <ErrorMessage />
+  ) : movies.length > 0 ? (
+    <MovieGrid movies={movies} onSelect={handleMovieSelect} />
+        ) : null}
+        {selectedMovie && (
+  <MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
+)}
+
+</main>
+    </div>
   );
 }
