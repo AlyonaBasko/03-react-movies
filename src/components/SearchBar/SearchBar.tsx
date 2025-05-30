03acfa8309315e849c1093093dceca26cf47f95a
alona <alenabask93@gmail.com>
2025-05-30 21:27:43 +0200

    update searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { type FormEvent, useState } from 'react';
+
 import css from './SearchBar.module.css';
 import { toast } from 'react-hot-toast';
 
@@ -7,24 +7,19 @@ interface SearchBarProps {
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const [query, setQuery] = useState('');
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
+  const handleAction = (formData: FormData) => {
     const searchQuery = formData.get('query') as string;
 
-    if (!searchQuery.trim()) {
+    if (!searchQuery || !searchQuery.trim()) {
       toast.error('Please enter your search query.');
       return;
     }
 
     onSubmit(searchQuery.trim());
-    e.currentTarget.reset();
-    setQuery('');
-  };
 
+    const form = document.querySelector(`.${css.form}`) as HTMLFormElement;
+    form?.reset();
+  };
   return (
     <header className={css.header}>
       <div className={css.container}>
@@ -36,7 +31,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
         >
           Powered by TMDB
         </a>
-        <form className={css.form} onSubmit={handleSubmit}>
+        <form className={css.form} action={handleAction}>
           <input
             className={css.input}
             type="text"
@@ -44,8 +39,6 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
             autoComplete="off"
             placeholder="Search movies..."
             autoFocus
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
           />
           <button className={css.button} type="submit">
             Search
